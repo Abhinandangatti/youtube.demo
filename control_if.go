1b33d94534df033e5268e6471bc8d89fe517affe
Abhinandan <agatti1997@gmail.com>
2018-01-23 12:06:06 +0530

    More changes to Control_if

--- control_if.go
+++ control_if.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    var a int = 30
-    if a > 20 {
-       fmt.Printf("Greater than 20")
-    } else if a == 20 {
-         fmt.Printf("Equals to 20")
-    } else {
-    fmt.Printf("Less than 20")
-    }
+	var a int = 30
+	if a > 20 {
+		fmt.Printf("Greater than 20") //Hello I am here
+	} else if a == 20 {
+		fmt.Printf("Equals to 20")
+	} else {
+		fmt.Printf("Less than 20")
+	}
 }
